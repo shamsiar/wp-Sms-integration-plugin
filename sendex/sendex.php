a86665174c2500cda1a4278944c3d256e1c8e80b
Shamsiar Hamid <shamsiarhamid@gmail.com>
2020-09-11 12:17:29 +0600

    Add files via upload

---
+++ sendex/sendex.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link               http://menusms.com/dongido
+ * @since             1.0.0
+ * @package           Sendex
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SENDEXPLEXUS SMS
+ * Description:       This is a SMS integration plugin for sending sms and otp verfication.
+ * Version:           1.0.0
+ * Author:            Shams
+ * Text Domain:       sendex
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'SENDEX_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-sendex-activator.php
+ */
+function activate_sendex() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sendex-activator.php';
+	Sendex_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-sendex-deactivator.php
+ */
+function deactivate_sendex() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sendex-deactivator.php';
+	Sendex_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_sendex' );
+register_deactivation_hook( __FILE__, 'deactivate_sendex' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-sendex.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_sendex() {
+
+	$plugin = new Sendex();
+	$plugin->run();
+
+}
+run_sendex();
